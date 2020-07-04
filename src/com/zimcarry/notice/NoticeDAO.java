98b0fa5cde5f5f498b8e4e1bed0f1ac1c499a0f5
duchang24 <dongki12345671@gmail.com>
2020-07-04 18:46:43 +0900

    java file commit

---
+++ src/com/zimcarry/notice/NoticeDAO.java
@@ -0,0 +1,80 @@
+package com.zimcarry.notice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+
+import com.zimcarry.db.DBConn;
+
+
+public class NoticeDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public List<NoticeDTO> getNoticeList() {
+		
+		List<NoticeDTO> noticeList = new ArrayList<NoticeDTO>();
+		try {
+			conn = DBConn.getConnection();
+			String sql = "SELECT no_idx, no_title, no_writer, no_writedate, no_hit, no_hidden FROM tb_notice";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				NoticeDTO notice = new NoticeDTO();
+				notice.setNoIdx(rs.getLong("no_idx"));
+				notice.setNoTitle(rs.getString("no_title"));
+				notice.setNoWriter(rs.getString("no_writer"));
+				notice.setNoWritedate(rs.getDate("no_writedate"));
+				notice.setNoHit(rs.getLong("no_hit"));
+				notice.setNoHidden(rs.getString("no_hidden"));
+				noticeList.add(notice);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBConn.close(conn, pstmt, rs);
+		}
+		return noticeList;
+	}
+	
+	public String noticeDataToJSON(String no_idx) {
+		String no_title = "", no_content = "", no_writer = "", no_writedate = "", no_hit = "", no_file = "", no_hidden = "";
+		try {
+			conn = DBConn.getConnection();
+			String sql = "SELECT no_title, no_content, no_writer, no_writedate, no_hit, no_file, no_hidden FROM tb_notice WHERE no_idx = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, no_idx);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				no_title = rs.getString("no_title");
+				no_content = rs.getString("no_content");
+				no_writer = rs.getString("no_writer");
+				no_writedate = rs.getString("no_writedate");
+				no_hit = rs.getString("no_hit");
+				no_file = rs.getString("no_file");
+				no_hidden = rs.getString("no_hidden");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBConn.close(conn, pstmt, rs);
+		}
+		JSONObject jobj = new JSONObject();
+		jobj.put("no_title", no_title);
+		jobj.put("no_content", no_content);
+		jobj.put("no_writer", no_writer);
+		jobj.put("no_writedate", no_writedate);
+		jobj.put("no_hit", no_hit);
+		jobj.put("no_file", no_file);
+		jobj.put("no_hidden", no_hidden);
+		
+		return jobj.toString();
+	}
+}
---
+++ src/com/zimcarry/notice/NoticeDTO.java
@@ -0,0 +1,63 @@
+package com.zimcarry.notice;
+
+import java.util.Date;
+
+public class NoticeDTO {
+	private long noIdx;
+	private String noTitle;
+	private String noContent;
+	private String noWriter;
+	private Date noWritedate;
+	private long noHit;
+	private String noFile;
+	private String noHidden;
+	
+	public long getNoIdx() {
+		return noIdx;
+	}
+	public void setNoIdx(long noIdx) {
+		this.noIdx = noIdx;
+	}
+	public String getNoTitle() {
+		return noTitle;
+	}
+	public void setNoTitle(String noTitle) {
+		this.noTitle = noTitle;
+	}
+	public String getNoContent() {
+		return noContent;
+	}
+	public void setNoContent(String noContent) {
+		this.noContent = noContent;
+	}
+	public String getNoWriter() {
+		return noWriter;
+	}
+	public void setNoWriter(String noWriter) {
+		this.noWriter = noWriter;
+	}
+	public Date getNoWritedate() {
+		return noWritedate;
+	}
+	public void setNoWritedate(Date noWritedate) {
+		this.noWritedate = noWritedate;
+	}
+	public long getNoHit() {
+		return noHit;
+	}
+	public void setNoHit(long noHit) {
+		this.noHit = noHit;
+	}
+	public String getNoFile() {
+		return noFile;
+	}
+	public void setNoFile(String noFile) {
+		this.noFile = noFile;
+	}
+	public String getNoHidden() {
+		return noHidden;
+	}
+	public void setNoHidden(String noHidden) {
+		this.noHidden = noHidden;
+	}
+}
