a34658f87d6bdedd294de657d02c41eda12d6072
minsung <hwannn1327@naver.com>
2020-07-06 14:52:12 +0900

    review

--- src/com/zimcarry/review/ReviewDTO.java
+++ src/com/zimcarry/review/ReviewDTO.java
@@ -4,11 +4,11 @@ import java.util.Date;
 
 public class ReviewDTO {
 	private long reIdx;
-	private int reScore;
+	private double reScore;
 	private String reTitle;
 	private String reContent;
 	private String reWriter;
 	private Date reWritedate;
 	private long reBookIdx;
-	
+
 }
